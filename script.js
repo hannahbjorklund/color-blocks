6201984431046af421bfea60209faa6fc5184777
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-10-26 14:43:13 -0500

    Made a function to track how many times a red block is added, and displays in html

--- script.js
+++ script.js
@@ -4,33 +4,46 @@ function clickRed(){
     console.log("You clicked red");
     let red = document.getElementById("blocks");
     red.innerHTML += "<div onclick='deleteRed(event)' class ='block red-fill'></div>";
-
+    let redCount = document.getElementById("red-count");
+    redCount.innerHTML = `${Number(redCount.innerHTML)+1}`;
 }
 
 function clickBlue(){
+    
     console.log("You clicked blue");
     let blue = document.getElementById("blocks");
     blue.innerHTML += "<div onclick='deleteBlue(event)' class='block blue-fill'></div>";
+
 }
 
 function clickGreen(){
     console.log("You clicked green");
     let green = document.getElementById("blocks");
-    green.innerHTML += "<div class='block green-fill'></div>";
+    green.innerHTML += "<div onclick='deleteGreen(event)' class='block green-fill'></div>";
 }
 
 function clickYellow(){
     console.log("You clicked yellow");
     let yellow = document.getElementById("blocks");
-    yellow.innerHTML += "<div class='block yellow-fill'></div>";
+    yellow.innerHTML += "<div onclick='deleteYellow(event)' class='block yellow-fill'></div>";
 }
 
-function deleteRed(event){
-    console.log("Deleting red");
-    event.target.remove();
-}
+// function deleteRed(event){
+//     console.log("Deleting red");
+//     event.target.remove();
+// }
 
-function deleteBlue(event){
-    console.log("Deleting blue");
-    event.target.remove();
-}
+// function deleteBlue(event){
+//     console.log("Deleting blue");
+//     event.target.remove();
+// }
+
+// function deleteGreen(event){
+//     console.log("Deleting green");
+//     event.target.remove();
+// }
+
+// function deleteYellow(event){
+//     console.log("Deleting yellow");
+//     event.target.remove();
+// }
