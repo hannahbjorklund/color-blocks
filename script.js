a9d1fbe77209bd4df1eae8cec670ac18b2e6e5e0
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-10-26 16:32:54 -0500

    Made adjustments to the way functions add or subtract from the block counts

--- script.js
+++ script.js
@@ -1,61 +1,66 @@
 console.log('🟥 🟦 🟩 🟨');
+let redBlockCounter = 1;
+let blueBlockCounter = 1;
+let greenBlockCounter = 1;
+let yellowBlockCounter = 1;
 
 function clickRed(){
     console.log("You clicked red");
     let red = document.getElementById("blocks");
     red.innerHTML += "<div onclick='deleteRed(event)' class ='block red-fill'></div>";
-    let redCount = document.getElementById("red-count");
-    redCount.innerHTML = `${Number(redCount.innerHTML)+1}`;
+    redBlockCounter++;
+    document.getElementById("red-count").textContent = redBlockCounter;
+    
 }
 
 function clickBlue(){
     console.log("You clicked blue");
     let blue = document.getElementById("blocks");
     blue.innerHTML += "<div onclick='deleteBlue(event)' class='block blue-fill'></div>";
-    let blueCount = document.getElementById("blue-count");
-    blueCount.innerHTML = `${Number(blueCount.innerHTML)+1}`;
+    blueBlockCounter++;
+    document.getElementById("blue-count").textContent = blueBlockCounter;
 }
 
 function clickGreen(){
     console.log("You clicked green");
     let green = document.getElementById("blocks");
     green.innerHTML += "<div onclick='deleteGreen(event)' class='block green-fill'></div>";
-    let greenCount = document.getElementById("green-count");
-    greenCount.innerHTML = `${Number(greenCount.innerHTML)+1}`;
+    greenBlockCounter++;
+    document.getElementById("green-count").textContent = greenBlockCounter;
 }
 
 function clickYellow(){
     console.log("You clicked yellow");
     let yellow = document.getElementById("blocks");
     yellow.innerHTML += "<div onclick='deleteYellow(event)' class='block yellow-fill'></div>";
-    let yellowCount = document.getElementById("yellow-count");
-    yellowCount.innerHTML = `${Number(yellowCount.innerHTML)+1}`;
+    yellowBlockCounter++;
+    document.getElementById("yellow-count").textContent = yellowBlockCounter;
 }
 
 function deleteRed(event){
     console.log("Deleting red");
     event.target.remove();
-    let redCount = document.getElementById("red-count");
-    redCount.innerHTML = `${Number(redCount.innerHTML)-1}`;
+    redBlockCounter--;
+    document.getElementById("red-count").textContent = redBlockCounter;
 }
 
 function deleteBlue(event){
     console.log("Deleting blue");
     event.target.remove();
-    let blueCount = document.getElementById("blue-count");
-    blueCount.innerHTML = `${Number(blueCount.innerHTML)-1}`;
+    blueBlockCounter--;
+    document.getElementById("blue-count").textContent = blueBlockCounter;
 }
 
 function deleteGreen(event){
     console.log("Deleting green");
     event.target.remove();
-    let greenCount = document.getElementById("green-count");
-    greenCount.innerHTML = `${Number(greenCount.innerHTML)-1}`;
+    greenBlockCounter--;
+    document.getElementById("green-count").textContent = greenBlockCounter;
 }
 
 function deleteYellow(event){
     console.log("Deleting yellow");
     event.target.remove();
-    let yellowCount = document.getElementById("yellow-count");
-    yellowCount.innerHTML = `${Number(yellowCount.innerHTML)-1}`;
+    yellowBlockCounter--;
+    document.getElementById("yellow-count").textContent = yellowBlockCounter;
 }
