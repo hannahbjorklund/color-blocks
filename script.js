b9f1e480b3e2964810ea152fd8b73631469baec5
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-10-26 14:20:36 -0500

    Created and tested functions to create a red, blue, or green block and used onclick in html so hitting the button calls the function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log('🟥 🟦 🟩 🟨');
+
+
+function clickRed(event){
+    console.log("You clicked red");
+    let red = document.getElementById("blocks");
+    red.innerHTML += "<div class ='block red-fill'></div>";
+}
+
+function clickBlue(event){
+    console.log("You clicked blue");
+    let blue = document.getElementById("blocks");
+    blue.innerHTML += "<div class='block blue-fill'></div>";
+}
+
+function clickGreen(event){
+    console.log("You clicked green");
+    let green = document.getElementById("blocks");
+    green.innerHTML += "<div class='block green-fill'></div>";
+}
+
