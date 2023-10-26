09a6fc2f4bda3506ec034aac86e52da796019ff5
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-10-26 14:28:03 -0500

    Added function for adding yellow squares upon button click. Started working on functions to delete the given block if the block is clicked

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 console.log('🟥 🟦 🟩 🟨');
 
-
-function clickRed(event){
+function clickRed(){
     console.log("You clicked red");
     let red = document.getElementById("blocks");
-    red.innerHTML += "<div class ='block red-fill'></div>";
+    red.innerHTML += "<div onclick='deleteRed(event)' class ='block red-fill'></div>";
+
 }
 
-function clickBlue(event){
+function clickBlue(){
     console.log("You clicked blue");
     let blue = document.getElementById("blocks");
-    blue.innerHTML += "<div class='block blue-fill'></div>";
+    blue.innerHTML += "<div onclick='deleteBlue(event)' class='block blue-fill'></div>";
 }
 
-function clickGreen(event){
+function clickGreen(){
     console.log("You clicked green");
     let green = document.getElementById("blocks");
     green.innerHTML += "<div class='block green-fill'></div>";
 }
 
+function clickYellow(){
+    console.log("You clicked yellow");
+    let yellow = document.getElementById("blocks");
+    yellow.innerHTML += "<div class='block yellow-fill'></div>";
+}
+
+function deleteRed(event){
+    console.log("Deleting red");
+    event.target.remove();
+}
+
+function deleteBlue(event){
+    console.log("Deleting blue");
+    event.target.remove();
+}
